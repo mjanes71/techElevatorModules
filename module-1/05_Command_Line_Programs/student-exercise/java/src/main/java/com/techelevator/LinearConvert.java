71e2f18fcebd24afe7f9cb8c958c819c9742b87d
Megan Janes <mjanes71@gmail.com>
2020-09-18 16:50:14 -0400

    Completed Temp and Linear Convert

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,30 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		System.out.println("Well hello there! Welcome to the length converter!");
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Please enter a numerical length for us to convert: ");
+		String lengthFromUser = input.nextLine();
+		int lengthToConvert = Integer.parseInt(lengthFromUser);
+
+		System.out.print("Thanks for that! Was that a length in (M)eters or (F)eet?: ");
+		String metersOrFeet = input.nextLine();
+
+		boolean isMeters = metersOrFeet.contains("M");
+
+		if(isMeters){
+			double metersForYou = lengthToConvert * 3.2808399;
+			System.out.println("Great! " + lengthFromUser + " meters is equal to " + metersForYou + " feet!");
+		}else {
+			double feetForYou = lengthToConvert * 0.3048;
+			System.out.println("Great! " + lengthFromUser + " feet is equal to " + feetForYou + " meters!");
+		}
 
 	}
 
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,8 +1,35 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		System.out.print("Enter the temperature you want to convert:");
+
+		Scanner input = new Scanner(System.in);
+
+		String temperature = input.nextLine();
+
+		System.out.print("Is the temperature in Celsius or Fahrenheit (enter C or F)");
+
+		String fahrenheitOrCelsius = input.nextLine();
+
+		boolean itsFahrenheit = fahrenheitOrCelsius.contains("F");
+
+
+		if (itsFahrenheit){
+			int fahrenheitTemp = Integer.parseInt(temperature);
+			double convertToCelsius = (fahrenheitTemp - 32) / 1.8;
+			System.out.println(temperature + " degrees Fahrenheit is " + convertToCelsius + "degrees Celsius." );
+		} else {
+			int celsiusTemp = Integer.parseInt(temperature);
+			double convertToFahrenheit = (celsiusTemp * 1.8 + 32);
+			System.out.println(temperature + " degrees Celsius is " + convertToFahrenheit + "degrees Fahrenheit.");
+		}
+
+
+
 
 	}
 
