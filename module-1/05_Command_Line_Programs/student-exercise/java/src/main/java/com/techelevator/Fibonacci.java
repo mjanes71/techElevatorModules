c522ac66ff18ce55d226cec958eedab56f6c0596
Megan Janes <mjanes71@gmail.com>
2020-09-19 15:39:43 -0400

    Finished Fibonacci program.

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,38 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to the Fibonacci sequence program!");
 
-	}
+		System.out.print("Please enter a Fibonacci number: ");
+		String userEntered = input.nextLine();
+		int fibNumber = Integer.parseInt(userEntered);
 
-}
+		int numPrev = 0;
+		int numNext = 1;
+
+		for (int i = numPrev; numPrev < fibNumber; i = numNext){
+			System.out.print(numPrev + ", ");
+			int sumOfPrev = numPrev + numNext;
+			numPrev = numNext;
+			numNext = sumOfPrev;
+
+		}
+
+
+
+
+		}
+
+
+
+
+		}
+
+
+
+
