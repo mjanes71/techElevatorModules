32a2ab808a4fbbcd21a6dc1ea5888134eab8ddda
Megan Janes <mjanes71@gmail.com>
2020-09-21 11:52:33 -0400

    Lecture notes on introduction to objects

--- module-1/06_Introduction_Objects_Strings/student-lecture/java/src/main/java/com/techelevator/Lecture.java
+++ module-1/06_Introduction_Objects_Strings/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -4,69 +4,85 @@ public class Lecture {
 
 	//1. Return any String
 	public String returnsAString() {
-		return null;
+		return "a string";//don't have to declare this string but could never reference it again
 	}
 
 	//2. getInputLength("abc") -> 3
 	public int getInputLength(String input) {
-		return -1;
+		return input.length();//have to put put the paren after the method
 	}
 
 	//3. getFirst3Letters("abcdef") -> "abc"
 	public String getFirst3Letters(String input) {
-		return new String();
-	}
+		return input.substring(0,3);
+	}//first param is index you want to start at, second param is index you want to stop at
+	//second param is NOT inclusive, but first param is (so 0,3 includes 0,1,2 but not 3)
 
 	//4. getLast3Letters("abcdef") -> "def"
 	public String getLast3Letters(String input) {
-		return "";
-	}
+		return input.substring(input.length()-3, input.length());
+	} //second param is optional, if left off, will just include all till end
+	// use .length when length of string being passed in is unknown
 
 	//5. removeFirstLetter("abcdef") -> "bcdef"
 	public String removeFirstLetter(String input) {
-		return "";
+		return input.substring(1);
 	}
 
 	//6. locationOfFirstCat("I have a catalog of cats.") -> 9
 	public int locationOfFirstCat(String stringWithCat) {
-		return Integer.MAX_VALUE;
+		return stringWithCat.indexOf("cat");
 	}
 
 	//7. locationOfLastCat("I have a catalog of cats.") -> 20
 	public int locationOfLastCat(String stringWithCat) {
-		return Integer.MIN_VALUE;
+		return stringWithCat.lastIndexOf("cat");
 	}
 
 	//8. getFifthCharacter("abcdef") -> 'e'
 	public char getFifthCharacter(String input) {
-		return 'x';
+		return input.charAt(4);
 	}
 
 	//9. isCareful("I don't think Voldemort is real.") -> "No"
 	//   isCareful("Beware of He Who Must Not Be Named.") -> "Yes"
 	public String isCareful(String statement) {
-		return "Yes";
+		boolean notCareful = statement.contains("Voldemort");
+		if(notCareful){
+			return "No";
+		}else {
+			return "Yes";
+		}
+
+
 	}
 
 	//10. roundOnBothEnds("Ohio") -> true
 	//    roundOnBothEnds("Oklahoma") -> false
 	public boolean roundOnBothEnds(String name) {
-		return false;
+		boolean startsWithO = name.toLowerCase().startsWith("o"); // this is how to run multiple methods at the same time
+		boolean endsWithO = name.toLowerCase().endsWith("o");
+		boolean allRound = startsWithO && endsWithO;
+		return allRound;
+
+
+
 	}
 
 	//11. sayItLouder("Look out!") -> "LOOK OUT!"
 	public String sayItLouder(String phrase) {
-		return phrase;
+		return phrase.toUpperCase();
 	}
 
 	//12. princeToFrog("The prince sat by the pond.") -> "The frog sat by the pond. "
 	public String princeToFrog(String story) {
-		return "";
+		return story.replace("prince","frog");
 	}
 
 	//13. isDarthVader("Anakin", "Skywalker") -> true
 	public boolean isDarthVader(String firstName, String lastName) {
-		return false;
+		return firstName.equals("Anakin") && lastName.equals("Skywalker");
+		//can also use .equalsIgnoreCase
 	}
 
 
