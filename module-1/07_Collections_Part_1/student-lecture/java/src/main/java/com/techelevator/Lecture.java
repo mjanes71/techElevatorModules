dc2002a4f8ac479bec59b10b66099b1967393ddd
Megan Janes <mjanes71@gmail.com>
2020-09-22 12:27:07 -0400

    Lecture notes on collections part 1

--- module-1/07_Collections_Part_1/student-lecture/java/src/main/java/com/techelevator/Lecture.java
+++ module-1/07_Collections_Part_1/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -9,54 +9,77 @@ public class Lecture {
 
 	//1. returnsAnArray(5) -> {1, 2, 3, 4, 5}
 	public int[] returnsAnArray(int number) {
-		return null;
+		int[] result = new int[number];
+		for (int i = 0; i < number; i++){
+			result[i] = i + 1;
+		}
+		return result;
 	}
 
 	//2. returnsAList(5) -> [1, 2, 3, 4, 5]
 	public List<Integer> returnsAList(int number) {
-		return null;
+		List<Integer> result = new ArrayList<>();
+		for(int i = 0; i < number; i++){
+			result.add(i+1);
+		}
+		return result;
 	}
 
 	//3. returnThirdName(["Frodo","Sam","Merry","Pippin"]) -> "Merry"
 	public String returnThirdName(List<String> names) {
-		return null;
+		return names.get(2);
+
 	}
 
 	//4. insertAfterFirst(["Luke","Leia"], "Han") -> ["Luke", "Han", "Leia"]
 	public List<String> insertAfterFirst(List<String> names, String name) {
-		return null;
+		List<String> result = new ArrayList<>(names); //makes the new list a copy of the list names
+		result.add(1, name); // we are changing the copy, not the input list- which reduces side effects
+		return result; //.add returns a boolean, not the list
 	}
 
 	//5. onlyOneAnswer(["yes", "no", "maybe"]) -> false
 	//   onlyOneAnswer(["no"]) -> true
 	public boolean onlyOneAnswer(List<String> answers) {
-		return false;
+		boolean oneAnswer = answers.size() == 1;
+		return oneAnswer;
 	}
 
 	//6. removeLastNumber([1.5, 2.2, 0.9]) -> [1.5, 2.2]
 	public List<Double> removeLastNumber(List<Double> numbers) {
-		return null;
+		numbers.remove(numbers.size()-1);
+		return numbers; //we did change the passed in list, so creating a copy would have been best practice here
 	}
 
 	//7. hasACow(["duck","cow","chicken"]) -> true
 	//   hasACow(["turkey", "rooster"]) -> false
 	public boolean hasACow(List<String> farmAnimals) {
-		return false;
+		boolean result = farmAnimals.contains("cow");
+		return result;
 	}
 
 	//8. yourPlaceInLine(["customer","customer","you","customer"]) -> 3
 	public int yourPlaceInLine(List<String> allCustomers) {
-		return -1;
+		int you = allCustomers.indexOf("you");
+		return you + 1; // have to add  one because indexOf returns the index, not the literal "place"
 	}
 
 	//9. trimArray({"a","b","c","d"}) -> {"b","c"}
 	public String[] trimArray(String[] input) {
-		return null;
+		List<String> copy = new ArrayList<>(Arrays.asList(input)); //last piece makes an array of the list, rest makes a new list that you can actually use
+		copy.remove(0);
+		copy.remove(copy.size()-1);
+		String[] result = copy.toArray(new String[0]); // turns list back to array
+		return result;
 	}
 
 	//10. descendingOrder([3, 4, 2, 9, 4, 8]) -> [9, 8, 4, 4, 3, 2]
 	public List<Integer> descendingOrder(List<Integer> numbers) {
-		return null;
+		List<Integer> result = new ArrayList<>(numbers);
+		Collections.sort(result);
+		Collections.reverse(result);
+		return result;
+
 	}
 
 	//11. countTrues([false,true,true,false,true]) -> 3
