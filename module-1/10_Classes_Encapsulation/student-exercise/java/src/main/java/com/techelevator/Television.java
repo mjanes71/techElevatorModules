1719071f7441f829539e55ef86e9c59fc3b06fa9
Megan Janes <mjanes71@gmail.com>
2020-09-24 16:23:19 -0400

    finished television class

--- module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
+++ module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,78 @@
 package com.techelevator;
 
 public class Television {
+    private boolean on = false;
+    private int currentChannel = 3;
+    private int currentVolume = 2;
+
+    public Television(){
+
+    }
+
+    public boolean isOn(){
+        return on;
+    }
+
+    public int getCurrentChannel(){
+        return currentChannel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void turnOff(){
+        this.on = false;
+    }
+
+    public void turnOn(){
+        this.on = true;
+        this.currentChannel = 3;
+        this.currentVolume = 2;
+    }
+
+    public void changeChannel(int newChannel){
+        if(this.on == true) {
+            if (newChannel >= 3 && newChannel <= 18) {
+                this.currentChannel = newChannel;
+            }
+        }
+    }
+
+    public void channelUp(){
+        if(this.on == true){
+            if(getCurrentChannel() == 18){
+                this.currentChannel = 3;
+            }else{
+                this.currentChannel = getCurrentChannel() + 1;
+            }
+        }
+    }
+
+    public void channelDown(){
+        if(this.on == true){
+            if(getCurrentChannel() == 3){
+                this.currentChannel = 18;
+            }else{
+                this.currentChannel = getCurrentChannel() - 1;
+            }
+        }
+    }
+
+    public void raiseVolume(){
+        if(this.on == true){
+            if(getCurrentVolume() < 10){
+                this.currentVolume = getCurrentVolume() + 1;
+            }
+        }
+    }
+
+    public void lowerVolume(){
+        if(this.on == true){
+            if(getCurrentVolume() > 0){
+                this.currentVolume = getCurrentVolume() - 1;
+            }
+        }
+    }
 
 }
