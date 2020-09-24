5e7184ab0f8752125c2fb7cdc809c874b3694eb5
Megan Janes <mjanes71@gmail.com>
2020-09-24 16:35:53 -0400

    finished elevator class

--- module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Elevator.java
+++ module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Elevator.java
@@ -1,5 +1,48 @@
 package com.techelevator;
 
 public class Elevator {
+    private int currentFloor;
+    private int numberOfFloors;
+    private boolean doorOpen;
 
+   public Elevator(int numberOfLevels){
+       this.numberOfFloors = numberOfLevels;
+       this.currentFloor = 1;
+   }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public boolean isDoorOpen() {
+        return doorOpen;
+    }
+
+    public void openDoor(){
+       this.doorOpen = true;
+    }
+
+    public void closeDoor(){
+       this.doorOpen = false;
+    }
+
+    public void goUp (int desiredFloor){
+       if(isDoorOpen()==false){
+           if(desiredFloor <= numberOfFloors && desiredFloor > getCurrentFloor()){
+               this.currentFloor = desiredFloor;
+           }
+       }
+    }
+
+    public void goDown(int desiredFloor){
+        if(isDoorOpen()==false){
+            if(desiredFloor >= 1 && desiredFloor < getCurrentFloor()){
+                this.currentFloor = desiredFloor;
+            }
+        }
+    }
 }
