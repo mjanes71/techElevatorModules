8c372a146e5b4d83e595e96e27a4d3ed20bd524a
Megan Janes <mjanes71@gmail.com>
2020-09-24 14:36:20 -0400

    finished homework assignment class

--- module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -2,4 +2,49 @@ package com.techelevator;
 
 public class HomeworkAssignment {
 
+    private int earnedMarks;
+    private int possibleMarks;
+    private String submitterName;
+
+    public HomeworkAssignment(int possibleMarks, String submitterName){
+        this.possibleMarks = possibleMarks; //do this b/c there is no setter so we set it directly
+        this.submitterName = submitterName;
+    }
+
+    public int getEarnedMarks(){
+        return earnedMarks;
+    }
+
+    public void setEarnedMarks(int earnedMarks){
+        this.earnedMarks = earnedMarks;
+    }
+
+    public int getPossibleMarks(){
+        return possibleMarks;
+    }
+
+    public String getSubmitterName(){
+        return submitterName;
+    }
+
+    public String getLetterGrade(){
+        double percentage = (double)getEarnedMarks() / (double)getPossibleMarks() *100;
+        int percentageInt = (int) percentage;
+        String letterGrade;
+        if(percentageInt >= 90){
+            letterGrade = "A";
+        }else if (percentageInt >=80){
+            letterGrade = "B";
+        }else if (percentageInt >=70){
+            letterGrade = "C";
+        }else if (percentageInt >= 60){
+            letterGrade = "D";
+        }else{
+            letterGrade = "F";
+        }
+
+        return letterGrade;
+    }
+
+
 }
