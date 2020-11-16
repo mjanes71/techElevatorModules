00c209cc7ccccc1501b9a4f451e0abc5b4ae35e1
Megan Janes <mjanes71@gmail.com>
2020-11-16 16:53:13 -0500

    finished exercises for monday

--- module-3/06_Introduction_to_JavaScript/student-exercise/challenge-exercises.js
+++ module-3/06_Introduction_to_JavaScript/student-exercise/challenge-exercises.js
@@ -6,25 +6,86 @@
     evenness, and return the position of this number. _Keep in mind that your task is to help 
     Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)_
 
-		iqTest("2 4 7 8 10") → 3 //third number is odd, while the rest are even
-		iqTest("1 2 1 1") → 2 // second number is even, while the rest are odd
-		iqTest("") → 0 // there are no numbers in the given set
+        iqTest("2 4 7 8 10") → 3 //third number is odd, while the rest are even
+        iqTest("1 2 1 1") → 2 // second number is even, while the rest are odd
+        iqTest("") → 0 // there are no numbers in the given set
         iqTest("2 2 4 6") → 0 // all numbers are even, therefore there is no position of an odd number
 */
 
+function iqTest(numbers) {
+    let result = 0;
+    if (numbers.length > 1) {
+        const answers = numbers.split(' ');
+        let evenness = [];
+        for (let i = 0; i < answers.length; i++) {
+            if (answers[i] % 2 === 0) {
+                evenness.push('e');
+            } else {
+                evenness.push('o');
+            }
+        }
+        if (evenness.indexOf('e') === evenness.lastIndexOf('e')) {
+            result = evenness.indexOf('e') + 1;
+        } else if (evenness.indexOf('o') === evenness.lastIndexOf('o')) {
+            result = evenness.indexOf('o') + 1;
+        } else {
+            result = 0;
+        }
+    }
+    return result;
+
+}
+
+
+
 /*
-2. **titleCase** Write a function that will convert a string into title case, given an optional 
-    list of exceptions (minor words). The list of minor words will be given as a string with each 
-    word separated by a space. Your function should ignore the case of the minor words string -- 
+2. **titleCase** Write a function that will convert a string into title case, given an optional
+    list of exceptions (minor words). The list of minor words will be given as a string with each
+    word separated by a space. Your function should ignore the case of the minor words string --
     it should behave in the same way even if the case of the minor word string is changed.
 
 
 * First argument (required): the original string to be converted.
 * Second argument (optional): space-delimited list of minor words that must always be lowercase
-except for the first word in the string. The JavaScript tests will pass undefined when this 
+except for the first word in the string. The JavaScript tests will pass undefined when this
 argument is unused.
 
-		titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
-		titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+        titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+        titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
         titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
 */
+
+function titleCase(title, others) {
+    const titleArr = title.toLowerCase().split(' ');
+
+
+    let result = [];
+    for (let word of titleArr) {
+        const first = word.substring(0, 1).toUpperCase();
+        const rest = word.substring(1).toLowerCase();
+        result.push(`${first}${rest} `);
+    }
+
+    if (others !== undefined) {
+        const othersArr = others.toLowerCase().split(' ');
+        let finalTitle = '';
+        for (let i = 0; i < result.length; i++) {
+            if (othersArr.includes(result[i].toLowerCase().trim()) && i > 0) {
+                finalTitle += result[i].toLowerCase();
+            } else {
+                finalTitle += result[i];
+            }
+        }
+        return finalTitle.trim();
+
+    } else {
+        let otherwise = '';
+        for (let word of result) {
+            otherwise += `${word}`;
+        }
+        return otherwise.trim();
+
+    }
+
+}
+
--- module-3/06_Introduction_to_JavaScript/student-exercise/exercises.js
+++ module-3/06_Introduction_to_JavaScript/student-exercise/exercises.js
@@ -5,13 +5,14 @@
 		sumDouble(1, 2) → 3
 		sumDouble(3, 2) → 5
 		sumDouble(2, 2) → 8
-
+*/
 		function sumDouble(x, y) {
-			// do logic here
-			// return result;
+			if (x === y){
+				return (x+x)*2;
+			}
 			return x + y;
         }
-*/
+
 
 /*
 2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
@@ -21,6 +22,18 @@
 		hasTeen(20, 19, 10) → true
 		hasTeen(20, 10, 13) → true
 */
+function hasTeen(x, y, z){
+	let result = false;
+	if (x > 12 && x < 20) {
+		result = true;
+	} else if (y > 12 && y < 20) {
+		result = true;
+	}else if (z > 12 && z < 20) {
+		result = true;
+	}
+	return result;
+}
+
 
 /* 
 3. **lastDigit** Given two non-negative int values, return true if they have the same 
@@ -30,6 +43,12 @@
 		lastDigit(6, 17) → false
 		lastDigit(3, 113) → true
 */
+function lastDigit(x, y) {
+	if ((x - y) % 10 === 0 || (y - x) % 10 === 0){
+		return true;
+	}
+	return false;
+}
 
 /*
 4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
@@ -40,6 +59,16 @@
         seeColor("blueTimes") → "blue"
 */
 
+function seeColor (color){
+if (color.substring(0,3) === 'red') {
+	return 'red';
+} else if (color.substring(0,4) === 'blue') {
+	return 'blue';
+} else {
+	return '';
+}
+}
+
 /*
 5. **oddOnly** Write a function that given an array of integer of any length, filters out 
     the even number, and returns a new array of just the the odd numbers.
@@ -47,6 +76,16 @@
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
 		oddOnly([2, 4, 8, 32, 256]); → []
 */
+function oddOnly(arr) {
+	let oddArray = [];
+	for (let num of arr) {
+		if (num % 2 !== 0) {
+			oddArray.push(num);
+		}
+	}
+	return oddArray;
+
+}
 
 /*
 6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
@@ -56,6 +95,11 @@
 		frontAgain("edit") → false
 		frontAgain("ed") → true
 */
+function frontAgain(word){
+	let firstTwo = word.substring(0,2);
+	let lastTwo = word.substring(word.length -2);
+	return firstTwo === lastTwo;
+}
 
 /*
 7. **cigarParty** When squirrels get together for a party, they like to have cigars. 
@@ -69,6 +113,17 @@ or false otherwise.
 		cigarParty(70, true) → true
 */
 
+function cigarParty (cigars, weekend) {
+	let isSucess = false;
+	if (weekend && cigars > 39){
+		isSucess = true;
+	} else if (cigars > 39 && cigars < 61){
+		isSucess = true;
+	}
+
+	return isSucess;
+}
+
 /*
 8. **fizzBuzz** Because you know you can't live without it, FizzBuzz.
 
@@ -78,6 +133,18 @@ or false otherwise.
 		fizzBuzz(15) → "FizzBuzz"
 		fizzBuzz(8) → 8
 */
+function fizzBuzz(x) {
+	if (x % 3 === 0 && x % 5 === 0) {
+		return 'FizzBuzz';
+	} else if (x % 3 === 0) {
+		return 'Fizz';
+	} else if (x % 5 === 0) {
+		return 'Buzz';
+	}else {
+		return x;
+	}
+
+}
 
 /*
 9. **filterEvens** Write a function that filters an array to only include even numbers.
@@ -87,6 +154,17 @@ or false otherwise.
 	filterEvens([2, 4, 6]) → [2, 4, 6]
 	filterEvens([100, 8, 21, 24, 62, 9, 7]) → [100, 8, 24, 62]
 */
+function filterEvens(arr) {
+	let evenArray = [];
+	for (let num of arr) {
+		if (num % 2 === 0) {
+			evenArray.push(num);
+		}
+	}
+	return evenArray;
+
+}
+
 
 /*
 10. **filterBigNumbers** Write a function that filters numbers greater than or equal to 100.
@@ -95,6 +173,15 @@ or false otherwise.
 	filterBigNumbers([3, 2, 7, 1, -100, -120]) → []
 	filterBigNumbers([]) → []
 */
+function filterBigNumbers(arr){
+	let bigNumArr = [];
+	for (let num of arr) {
+		if (num >= 100) {
+			bigNumArr.push(num);
+		}
+	}
+	return bigNumArr;
+}
 
 /*
 11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a 
@@ -103,6 +190,15 @@ parameter, `x` passed in.
 	filterMultiplesOfX([3, 5, 1, 9, 18, 21, 42, 67], 3) → [3, 9, 18, 21, 42]
 	filterMultiplesOfX([3, 5, 10, 20, 18, 21, 42, 67], 5) → [5, 10, 20]
 */
+
+function filterMultiplesOfX(arr, x) {
+let multiplesArr = [];
+for (let num of arr) {
+	if (num % x === 0) {
+		multiplesArr.push(num);
+	}
+} return multiplesArr;
+}
 
 /*
 12. **createObject** Write a function that creates an object with a property called 
@@ -116,3 +212,12 @@ firstName, lastName, and age. Populate the properties with your values.
 		age
 	}
 */
+function createObject(){
+	const me = {
+		firstName: 'Megan',
+		lastName: 'Janes',
+		age: 31
+	};
+	return me;
+}
+
